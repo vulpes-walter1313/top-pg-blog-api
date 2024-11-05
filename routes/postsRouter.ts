f055b6bf1fc837828d1aabe1ba07304341d6294a
Walter Fritsche <wfritsche95@protonmail.com>
2024-11-05 09:53:19 -0600

    changed postId to postSlug

--- routes/postsRouter.ts
+++ routes/postsRouter.ts
@@ -6,12 +6,12 @@ const router = express.Router();
 // all routes are prefixed by /posts
 router.get("/", postController.postsGET);
 router.post("/", postController.postsPOST);
-router.get("/:postId", postController.postGET);
-router.put("/:postId", postController.postPUT);
-router.delete("/:postId", postController.postDELETE);
-router.get("/:postId/comments", postController.postCommentsGET);
-router.post("/:postId/comments", postController.postCommentsPOST);
-router.put("/:postId/comments/:commentId", postController.postCommentPUT);
-router.delete("/:postId/comments/:commentId", postController.postCommentDELETE);
+router.get("/:postSlug", postController.postGET);
+router.put("/:postSlug", postController.postPUT);
+router.delete("/:postSlug", postController.postDELETE);
+router.get("/:postSlug/comments", postController.postCommentsGET);
+router.post("/:postSlug/comments", postController.postCommentsPOST);
+router.put("/:postSlug/comments/:commentId", postController.postCommentPUT);
+router.delete("/:postSlug/comments/:commentId", postController.postCommentDELETE);
 
 export default router;
